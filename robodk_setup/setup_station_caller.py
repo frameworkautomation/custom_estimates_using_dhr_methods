ccf8e8cda8fce8783c3bd7c9e78de8b21ccfbf23
Samuel Strongin <sam@framework.co>
2026-04-30 13:11:13 -0700

    not sure why I deleted this

--- robodk_setup/setup_station_caller.py
+++
@@ -1,8 +0,0 @@
-"""Drop this file into RoboDK's Scripts folder once and leave it there.
-It calls the actual setup script from the repo so you never need to move it again.
-
-RoboDK Scripts folder is typically:
-    C:\RoboDK\Scripts\
-"""
-import runpy
-runpy.run_path(r"C:\Users\samst\Framework\clones\custom_estimates_using_dhr_methods\robodk_setup\setup_station.py")
